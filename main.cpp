e5219e2b95ac3ca0694393b74cc54a98a052d74b
giulio <giulio2908@gmail.com>
2026-05-27 19:01:41 +0200

    Tappa 04: Attacco del giocatore

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+#include <vector>
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Angle.hpp>
 #include <iostream>
 
 
@@ -7,13 +10,16 @@
 //////////////////////
 
 // window
-const char* window_title = "Tappa-03";
+const char* window_title = "Tappa-04";
 const unsigned window_width = 1200;
 const unsigned window_height = 680;
 const float max_frame_rate = 60;
 
 // Player
 const float player_speed = {400};
+
+// Bullet
+const float bullet_speed = {500};
 
 struct Room{
     sf::Texture texture;
@@ -34,23 +40,52 @@ struct Player{
     float speed;
 
     Player ();
-    void draw (int direction, sf::RenderWindow& window);
-    void move_left (float elapsed);
-    void move_right (float elapsed);
-    void move_up (float elapsed);
-    void move_down (float elapsed);
+    void draw (sf::RenderWindow& window);
+    void setDirection (int direction);
+    void move_player_left (float elapsed);
+    void move_player_right (float elapsed);
+    void move_player_up (float elapsed);
+    void move_player_down (float elapsed);
 };
 
+struct Bullet{
+    sf::Vector2f pos;
+    sf::Vector2f size;
+    sf::Texture texture;
+    int direction;
+    float speed;
+    bool inAction;
+
+    Bullet(int direction, sf::Vector2f posPlayer);
+    void draw (sf::RenderWindow& window);
+    void move_bullet (float elapsed);
+};
+
+struct Attack{
+    std::vector<Bullet> bullets;
+
+    Attack(){};
+    void draw (sf::RenderWindow& window);
+    void attack(int direction, sf::Vector2f posPlayer);
+    void update(float elapsed);
+};
 
 struct State{
     Room room;
     Player player;
+    Attack attack;
     bool focus;
     bool pause;
+
     bool move_player_left;
     bool move_player_right;
     bool move_player_up;
     bool move_player_down;
+    
+    int move_bullet_left;
+    int move_bullet_right;
+    int move_bullet_up;
+    int move_bullet_down;
 
     State() : room(), 
               focus(false), 
@@ -58,14 +93,18 @@ struct State{
               move_player_left(false), 
               move_player_right(false), 
               move_player_up(false), 
-              move_player_down(false) {}
+              move_player_down(false),
+              move_bullet_left(0), 
+              move_bullet_right(0), 
+              move_bullet_up(0), 
+              move_bullet_down(0) {}
     void draw (sf::RenderWindow& window);
     void update(float elapsed);
     void collision();
 };
 
 
-Room::Room() : sprite(texture){
+Room::Room () : sprite(texture){
     texture = sf::Texture("texture/floor/floorBlock01.png");
     texture.setRepeated(true);
     sprite = sf::Sprite(texture);
@@ -88,6 +127,28 @@ Player::Player ()
     speed = player_speed;
 }
 
+Bullet::Bullet (int direction, sf::Vector2f posPlayer)
+{
+    this->direction = direction;
+    
+    inAction = true;
+    texture = sf::Texture ("texture/bullet/bullet.png");
+    size = (sf::Vector2f)texture.getSize();
+    if(direction == 0){
+        pos = {posPlayer.x, posPlayer.y + (size.y)};
+    }
+    else if(direction == 1){
+        pos = {posPlayer.x, posPlayer.y - (size.y/2.f)};
+    }
+    else if(direction == 2){
+        pos = {posPlayer.x - (size.x/2.f), posPlayer.y};
+    }
+    else if(direction == 3){
+        pos = {posPlayer.x + (size.x), posPlayer.y};
+    }
+    speed = bullet_speed;
+}
+
 //////////
 // Draw //
 //////////
@@ -96,19 +157,7 @@ void Room::draw (sf::RenderWindow& window){
     window.draw(sprite);
 }
 
-void Player::draw (int direction, sf::RenderWindow& window){
-    if(direction == 0){
-        lastTexture = textureLeft;
-    }
-    else if(direction == 1){
-        lastTexture =  textureRight;
-    }
-    else if(direction == 2){
-        lastTexture = textureBack;
-    }
-    else if(direction == 3){
-        lastTexture = textureFront;
-    }
+void Player::draw (sf::RenderWindow& window){
     size = (sf::Vector2f)lastTexture.getSize();
     sf::RectangleShape p (size);
     p.setTexture(&lastTexture);
@@ -116,57 +165,135 @@ void Player::draw (int direction, sf::RenderWindow& window){
     window.draw(p);
 }
 
+void Bullet::draw (sf::RenderWindow& window){
+    if(!inAction){
+        return;
+    }
+
+    sf::RectangleShape bullet (size);
+    bullet.setTexture(&texture);
+    bullet.setPosition(pos);
+    
+    if(direction == 0){
+        bullet.setRotation(sf::Angle(sf::degrees(-135.f)));
+    }
+    else if(direction == 1){
+        bullet.setRotation(sf::Angle(sf::degrees(45.f)));
+    }
+    else if(direction == 2){
+        bullet.setRotation(sf::Angle(sf::degrees(-45.f)));
+    }
+    else if(direction == 3){
+        bullet.setRotation(sf::Angle(sf::degrees(135.f)));
+    }
+    
+    window.draw(bullet);
+}
+
+void Attack::draw (sf::RenderWindow& window){
+    for(auto& bullet : bullets){
+        bullet.draw(window);
+    }
+}
 
 void State::draw (sf::RenderWindow& window){
     room.draw(window);
     if(!pause){
-        if(move_player_left)
-            player.draw(0, window);
-        else if(move_player_right)
-            player.draw(1, window);
-        else if(move_player_up)
-            player.draw(2, window);
-        else if(move_player_down)
-            player.draw(3, window);
+        attack.draw(window);
     }
-        player.draw(-1, window);
+        player.draw(window);
 }
 
 ////////////
 // Update //
 ////////////
-void Player::move_left (float elapsed){
+void Player::setDirection(int direction){
+    if(direction == 0) lastTexture = textureLeft;
+    if(direction == 1) lastTexture = textureRight;
+    if(direction == 2) lastTexture = textureBack;
+    if(direction == 3) lastTexture = textureFront;
+}
+
+void Player::move_player_left (float elapsed){
     pos.x -= speed * elapsed;
 }
 
-void Player::move_right (float elapsed){
+void Player::move_player_right (float elapsed){
     pos.x += speed * elapsed;
 }
 
-void Player::move_up (float elapsed){
+void Player::move_player_up (float elapsed){
     pos.y -= speed * elapsed;
 }
 
-void Player::move_down (float elapsed){
+void Player::move_player_down (float elapsed){
     pos.y += speed * elapsed;
+}
+
+void Bullet::move_bullet (float elapsed){
+    if(direction == 0){
+        pos.x -= speed * elapsed;
+    }
+    else if(direction == 1){
+        pos.x += speed * elapsed;
+    }
+    else if(direction == 2){
+        pos.y -= speed * elapsed;
+    }
+    else if(direction == 3){
+        pos.y += speed * elapsed;
+    }
+}
+
+void Attack::attack (int direction, sf::Vector2f posPlayer){
+    bullets.push_back(Bullet(direction, posPlayer));
+}
+
+void Attack::update (float elapsed){
+    for(auto& bullet : bullets){
+        bullet.move_bullet(elapsed);
+    }
+    
 }
 
 void State::update (float elapsed){
     if(!pause){
-        if(move_player_left)
-            player.move_left(elapsed);
-        if(move_player_right)
-            player.move_right(elapsed);
-        if(move_player_up)
-            player.move_up(elapsed);
-        if(move_player_down)
-            player.move_down(elapsed);
+        if(move_player_left){
+            player.move_player_left(elapsed);
+            player.setDirection(0);
+        }
+        if(move_player_right){
+            player.move_player_right(elapsed);
+            player.setDirection(1);
+        }
+        if(move_player_up){
+            player.move_player_up(elapsed);
+            player.setDirection(2);
+        }
+        if(move_player_down){
+            player.move_player_down(elapsed);
+            player.setDirection(3);
+        }
         
+        // Player attact
+        for(move_bullet_left; move_bullet_left > 0; move_bullet_left--){
+            attack.attack(0, {player.pos.x + (player.size.x / 2.f), player.pos.y + (player.size.y / 2.f)});
+        }
+        for(move_bullet_right; move_bullet_right > 0; move_bullet_right--){
+            attack.attack(1, {player.pos.x + (player.size.x / 2.f), player.pos.y + (player.size.y / 2.f)});
+        }
+        for(move_bullet_up; move_bullet_up > 0; move_bullet_up--){
+            attack.attack(2, {player.pos.x + (player.size.x / 2.f), player.pos.y + (player.size.y / 2.f)});
+        }
+        for(move_bullet_down; move_bullet_down > 0; move_bullet_down--){
+            attack.attack(3, {player.pos.x + (player.size.x / 2.f), player.pos.y + (player.size.y / 2.f)});
+        }
+        attack.update(elapsed);
         collision();
     }
 }
 
-void State::collision(){
+void State::collision (){
     if(player.pos.x < 0.f){
         player.pos.x = 0.f;
     }
@@ -179,7 +306,8 @@ void State::collision(){
     if(player.pos.y + player.size.y > window_height){
         player.pos.y = window_height - player.size.y;
     }
-} 
+}
+
 ////////////
 // Events //
 ////////////
@@ -207,6 +335,8 @@ void handle (const sf::Event::KeyPressed& key, State& state)
     case sf::Keyboard::Scan::Enter:
         state.pause = !state.pause;
         return;
+
+    // Player movement
     case sf::Keyboard::Scan::A:
         state.move_player_left = true;
         return;
@@ -219,6 +349,21 @@ void handle (const sf::Event::KeyPressed& key, State& state)
     case sf::Keyboard::Scan::S:
         state.move_player_down = true;
         return;
+    
+    // Player attack (bullet movement)
+    case sf::Keyboard::Scancode::Left:
+        state.move_bullet_left ++;
+        return;
+    case sf::Keyboard::Scancode::Right:
+        state.move_bullet_right ++;
+        return;
+    case sf::Keyboard::Scancode::Up:
+        state.move_bullet_up ++;
+        return;
+    case sf::Keyboard::Scancode::Down:
+        state.move_bullet_down ++;
+        return;
+    
     default:
         return;
     }
@@ -230,6 +375,8 @@ void handle (const sf::Event::KeyReleased& key, State& state)
         return;
 
     switch (key.scancode) {
+        
+    // Player movement
     case sf::Keyboard::Scan::A:
         state.move_player_left = false;
         return;
@@ -242,6 +389,7 @@ void handle (const sf::Event::KeyReleased& key, State& state)
     case sf::Keyboard::Scan::S:
         state.move_player_down = false;
         return;
+    
     default:
         return;
     }
@@ -262,7 +410,7 @@ void handle (const sf::Event::FocusLost&, State& state)
 // Loop //
 //////////
 
-int main()
+int main ()
 {
     
     sf::RenderWindow window (sf::VideoMode({window_width, window_height}), window_title);
